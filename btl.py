77a924e24b9528d43b44cbb109df95ba7cafeb30
Stuart Abrams-Humphries <stuartabramshumphries@gmail.com>
2016-06-05 15:39:53 +0100

    fixed issue with remortgaging - now works correctly

--- btl.py
+++ btl.py
@@ -3,25 +3,27 @@
 
 
 def main():
-    ir = 0.05  # interest rates
-    gr = 0.03  # property growth rate
-    ltv = 0.60  # loan to value
-    ib = 140000  # initial deposit/balance
-    yrs = 10  # years to model
-    yld = 0.04  # rental yield
-    savings = 0  # amount added by savings
+    ir = 0.03       # interest rates
+    gr = 0.05       # property growth rate
+    ltv = 0.60      # loan to value
+    ib = 140000     # initial deposit/balance
+    yrs = 15         # years to model
+    yld = 0.04      # rental yield
+    savings = 0     # amount added by savings
 
     startval = ival = ib / (1 - ltv)
     for i in range(1, yrs + 1):
         ival += savings
+        mortg = startval * ltv
         ival = ival * (1 + gr)
-        if ival >= startval * (2 - ltv):
+        print ival,startval
+        if ival >= startval / ltv:
             print 'remortgaging as value increased enough:'
             newfunds = ltv * (ival - startval)
             ival = ival + newfunds / (1 - ltv)
-        startval = ival
-        rent = startval * yld - ltv * ir * startval
-        tax = (startval * yld - (0.2*ltv * ir * startval))*0.45
+            startval = ival
+        rent = ival * yld - mortg*ir
+        tax = (startval * yld - (0.2*ltv * ir * mortg))*0.45     # this is using the new bad taxes - assuming worst case
         netrent = rent -tax
         ival = ival + netrent
         print 'year\t\t%d\t totval %d   val %.2f rent %d netrent %d' % (i, ival,
