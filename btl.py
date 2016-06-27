0649d8e8e1ffe848c45c78e06fbfbe149be0f25a
Stuart Abrams-Humphries <stuartabramshumphries@gmail.com>
2016-06-27 18:20:43 +0100

    regular checkin

--- btl.py
+++ btl.py
@@ -1,55 +1,54 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+__author__ = "Stuart Abrams-Humphries (stuartabramshumphries@gmail.com)"
 
 def main():
-    ir = 0.0444       # interest rates
-    gr = 0.05       # property growth rate
-    ltv = 0.60     # loan to value
-    ib = 180000     # initial deposit/balance
-    yrs = 15       # years to model
-    yld = 0.0344      # rental yield (this is my personal value after agency fees/vacancies/costs)
-    savings = 0  # amount added by savings - note this messes up my interest rate achieved calc at the moment
+    ir=0.04  # interest rates
+    gr=0.04  # property growth rate
+    ltv=0.01; # loan to value
+    ib=180000  # initial deposit/balance
+    yrs=5  # years to model
+    yld=0.03  # rental yield
+    savings=0  # amount added by savings - note this messes up my interest rate achieved calc at the moment
 
-    startval = ival = ib / (1 - ltv)
-    mortg = startval * ltv
+    startval=ival=ib / (1 - ltv)
     for i in range(1, yrs + 1):
-        ival += savings
-        ival = ival * (1.0 + gr)
+        ival+=savings
+        mortg=startval * ltv
+        ival=ival * (1.0 + gr)
         if ival >= startval / ltv:
-            print 'remortgaging to buy additonal property as value increased enough:'
-            newfunds = ltv * (ival - startval)
-            ival = ival + newfunds / (1 - ltv)
-            startval = ival
-            mortg = startval * ltv
-        rent = ival * yld - mortg * ir
-        # this is using the new bad taxes - assuming worst case
-        tax = (startval * yld - (0.2 * ir * mortg)) * 0.40
-        netrent = rent - tax
-        ival = ival + netrent
+            print 'remortgaging as value increased enough:'
+            newfunds=ltv * (ival - startval)
+            ival=ival + newfunds / (1 - ltv)
+            startval=ival
+            mortg=startval * ltv
+        rent=ival * yld - mortg * ir
+        tax=(startval * yld - (0.2 * ir * mortg)) * 0.45  # this is using the new bad taxes - assuming worst case
+        netrent=rent - tax
+        ival=ival + netrent
         print 'year\t\t%d\t totval %d   netval %.2f rent %d netrent %d' % (i, ival,
                                                                            ival * (1.0 - ltv), rent, netrent)
 
     class Calgr(object):
 
-        def __init__(self, ib, ival, yrs, netval):
-            self.ib = ib
-            self.ival = ival
-            self.yrs = yrs
-            self.ltv = ltv
-            self.netval = netval
+        def __init__(self, ib, ival, yrs, ltv):
+            self.ib=ib
+            self.ival=ival
+            self.yrs=yrs
+            self.ltv=ltv
 
         def irate(self):
-            # trying to get what the ir is given initial balance and final
-            # balance
+            # trying to get what the ir is given initial balance and final balance
             from math import pow
-            r = pow((self.netval / self.ib), 1.0 / self.yrs) - 1
-            r = r * 100
+            self.ival=self.ival * (1.0 - self.ltv)
+            r=pow((self.ival / self.ib), 1.0 / self.yrs) - 1
+            r=r * 100
             return r
 
-    netval = ival * (1.0 - ltv)
-    vaal = Calgr(ib, ival, yrs, netval)
-    valew = vaal.irate()
-    print("Interest rate achieved is %.2f%% this is after income taxes are paid, but not capital gains tax if sold") % valew
+    vaal=Calgr(ib, ival, yrs, ltv)
+    valew=vaal.irate()
+    print("Interest rate achieved is %.2f%%") % valew
+
 
 if __name__ == '__main__':
     main()
