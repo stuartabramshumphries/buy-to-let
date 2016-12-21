9c46d2ec02637a3ec59603da74c62a1117630131
Stuart Abrams-Humphries <stuartabramshumphries@gmail.com>
2016-12-21 17:26:51 +0000

    fixing for pep8

--- btl.py
+++ btl.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 # need to add more info here for other users or extend the README
 
-__author__ = "Stuart Abrams-Humphries"
-__email__ = "Stuart Abrams-Humphries (stuartabramshumphries@gmail.com)"
+__author__     = "Stuart Abrams-Humphries"
+__email__      = "Stuart Abrams-Humphries (stuartabramshumphries@gmail.com)"
 __maintainer__ = "Stuart Abrams-Humphries"
-__license__ = "GPL"
+__license__    = "GPL"
 
 
 def main():
     import os
-    ir = 0.035                     # interest rate
-    gr = 0.04                      # property growth rate
+    ir = 0.045                     # interest rate
+    gr = 0.055                      # property growth rate
     ltv = 0.75                     # loan to value
     ib = 100000                    # initial deposit/balance
     yrs = 20                       # years to model
@@ -28,24 +28,25 @@ def main():
         mortg = startval * ltv
         imortg = mortg
         ival *= (1.0 + gr)
-        if ival >= startval *1.2:
+        if ival >= startval * 1.2:
             print 'remortgaging as value increased enough:'
             newfunds = ltv * (ival - startval)
             ival += newfunds / (1 - ltv)
             startval = ival
             mortg = startval * ltv
         rent = ival * yld - mortg * ir
-        tax = (startval * yld - (0.2 * ir * mortg)) * 0.45  # this is using the new bad taxes - assuming worst case
+        # this is using the new bad taxes - assuming worst case
+        tax = (startval * yld - (0.2 * ir * mortg)) * 0.45
         netrent = rent - tax
         ival += netrent
         yvals.append(ival)
-        yvals2.append(ival -mortg)
+        yvals2.append(ival - mortg)
         print 'year\t\t%d\t totval %d   netval %.2f rent %d netrent %d' % (i, ival,
-                                                                           ival -mortg, rent, netrent)
+                                                                           ival - mortg, rent, netrent)
 
     class Calgr(object):
 
-        def __init__(self, ib, ival, yrs, ltv,imortg,savings):
+        def __init__(self, ib, ival, yrs, ltv, imortg, savings):
             self.ib = ib
             self.ival = ival
             self.yrs = yrs
@@ -54,19 +55,22 @@ def main():
             self.savings = savings
 
         def irate(self):
-            # trying to get what the ir is given initial balance and final balance
+            # trying to get what the ir is given initial balance and final
+            # balance
             from math import pow
-            r = pow(((self.ival -self.imortg -self.savings*self.yrs)/ self.ib), 1.0 / self.yrs) - 1
+            r = pow(
+                ((self.ival - self.imortg - self.savings * self.yrs) / self.ib),
+                1.0 / self.yrs) - 1
             r *= 100
             return r
 
-    vaal = Calgr(ib, ival, yrs, ltv,mortg,savings)
+    vaal = Calgr(ib, ival, yrs, ltv, mortg, savings)
     valew = vaal.irate()
     print "Interest rate achieved is %.4f%%" % valew
 
     class Graphit(object):
-    
-        def __init__(self, xvals, yvals,yvals2):
+
+        def __init__(self, xvals, yvals, yvals2):
             self.xvals = xvals
             self.yvals = yvals
             self.yvals2 = yvals2
@@ -82,13 +86,25 @@ def main():
             pyplot.title('portfolio growth over years')
             pyplot.xlabel('Years')
             pyplot.ylabel('portfolio value')
-            pyplot.plot(xvals,yvals,color='blue',linestyle='dashed',label='total value',linewidth=5.0)
-            pyplot.plot(xvals,yvals2,color='green',linestyle='dashed',label='net value',linewidth=5.0)
+            pyplot.plot(
+                xvals,
+                yvals,
+                color='blue',
+                linestyle='dashed',
+                label='total value',
+                linewidth=5.0)
+            pyplot.plot(
+                xvals,
+                yvals2,
+                color='green',
+                linestyle='dashed',
+                label='net value',
+                linewidth=5.0)
             pyplot.legend(loc='upper left')
             pyplot.savefig('./graph.png')
 
-    xvals = range(0,yrs)
-    graf = Graphit(xvals, yvals,yvals2)
+    xvals = range(0, yrs)
+    graf = Graphit(xvals, yvals, yvals2)
     graf.pretty()
     os.popen('(eog --new-instance ./graph.png)&')
 
